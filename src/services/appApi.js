fc9621f7f5a38a5e4dfd9500e115742efade0ad4
Ahmed Usama <ahmedusamasaad@gmail.com>
2023-05-22 14:35:47 +0200

    heelp

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "https://Ecommern-api.onrender.com" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "https://ecommern-api-6lrv.onrender.com" }),
     endpoints: (builder) => ({
         signup: builder.mutation({
             query: (user) => ({
