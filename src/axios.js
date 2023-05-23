00325c0d55dce895aafaeb1cd32faf4db980a875
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2023-05-23 05:05:13 +0200

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://ecommern-api-vxe0.onrender.com",
 });
 
 export default instance;
